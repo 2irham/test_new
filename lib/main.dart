6a21509424f6d457a21411bab568e88d244a4832
Irham <ikmuttaqin@gmail.com>
2022-07-09 14:11:48 +0700

    "Changed a little"

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Media Query',
+      title: 'Media Query Demo',
       home: HomePage(),
     );
   }
